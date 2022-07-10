d9766d91e4ded6f33226d02f2df158b2bbe4f757
Taavi-Univer <taavi.univer@gmail.com>
2022-07-10 17:56:01 +0300

    changed games.js

--- Simon-Game/game.js
+++ Simon-Game/game.js
@@ -1,8 +1,25 @@
 let userClickedPattern = [];
 let gamePattern = [];
+let level = 0;
+let started = false;
 const buttonColours = ["red", "blue", "green", "yellow"];
 
+// START THE GAME
+$(document).keypress(function () {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
 function nextSequence() {
+  userClickedPattern = [];
+
+  level++;
+
+  $("#level-title").text("Level " + level);
+
   let randomNumber = Math.floor(Math.random() * 4);
 
   let randomChosenColour = buttonColours[randomNumber];
@@ -21,7 +38,33 @@ $(".btn").click(function () {
   userClickedPattern.push(userChosenColour);
   playSound(userChosenColour);
   animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length - 1);
 });
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+    startOver();
+  }
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
 
 function playSound(name) {
   var audio = new Audio("sounds/" + name + ".mp3");
@@ -30,7 +73,7 @@ function playSound(name) {
 
 function animatePress(currentColour) {
   $("." + currentColour).addClass("pressed");
-  setTimeout(() => {
+  setTimeout(function () {
     $("." + currentColour).removeClass("pressed");
   }, 100);
 }
